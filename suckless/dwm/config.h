e9c924180cf9c4378ffed44f0affb913f036552f
antaxiom <40070530+antaxiom@users.noreply.github.com>
2020-08-24 09:24:56 -0400

    Changed color scheme

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -19,7 +19,7 @@ static const char col_gray1[]       = "#111111";
 static const char col_gray2[]       = "#222222";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#3085a1";
+static const char col_cyan[]        = "#409cba";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
