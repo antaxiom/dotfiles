0bafb62cf7e1944656814cf298735fb4aacfbaa3
antaxiom <40070530+antaxiom@users.noreply.github.com>
2020-08-24 10:41:21 -0400

    Change dmenu colors

--- suckless/dmenu-4.9/config.h
+++ suckless/dmenu-4.9/config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMonoNF:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSel] = { "#eeeeee", "#61afef" },
+	[SchemeOut] = { "#111111", "#61afef" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
