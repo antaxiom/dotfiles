5a941bf12caa92ad0fda6239eafd759bb5c9de56
antaxiom <40070530+antaxiom@users.noreply.github.com>
2020-08-29 16:31:00 -0400

    Added dwm autostart

---
+++ .dwm/autostart.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+wmname LG3D &
+picom &
+nitrogen --restore &
+dunst &
+killall dwmblocks
+~/suckless/dwmblocks/dwmblocks &
+killall sxhkd &
+sxhkd &
+# Setup secondary monitor
+xrandr --output HDMI-A-0 --mode 1920x1080 --same-as DisplayPort-2 --scale 2.0x2.0
