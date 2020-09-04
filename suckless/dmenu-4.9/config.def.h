b032321cfd59d3d70746d16670ec8c4d7c66af82
antaxiom <40070530+antaxiom@users.noreply.github.com>
2020-09-03 21:09:22 -0400

    Dmenu consistent font and color

--- suckless/dmenu-4.9/config.def.h
+++ suckless/dmenu-4.9/config.def.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMonoNF:size=12"
+	"JetBrainsMonoNF"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#61afef" },
+	[SchemeNorm] = { "#bbbbbb", "#111111" },
+	[SchemeSel] = { "#111111", "#61afef" },
 	[SchemeOut] = { "#111111", "#61afef" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
