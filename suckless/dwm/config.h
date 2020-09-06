7ae4b3db87d1acaf2c689892cb353b27938d49c3
antaxiom <40070530+antaxiom@users.noreply.github.com>
2020-09-05 21:41:19 -0400

    dmenu: big payoff, added prompt in dwm, fixed height

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -86,11 +86,11 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p", "run","-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_d,      spawn,          SHCMD("j4-dmenu-desktop --dmenu=\'dmenu -fn JetBrainsMonoNF -nb \"#111111\" -nf \"#bbbbbb\" -sb \"#61afef\" -sf \"#111111\" \' ") },
+	{ MODKEY,                       XK_d,      spawn,          SHCMD("j4-dmenu-desktop --dmenu=\'dmenu -p start\'")},
 	{ MODKEY,                       XK_r,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
