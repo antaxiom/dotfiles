5fcb2d61266bb72d21cb8b1f023cc42f3e797b82
antaxiom <40070530+antaxiom@users.noreply.github.com>
2020-09-13 21:05:31 -0400

    dwm: smaller borders

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 16;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 16;       /* vert inner gap between windows */
