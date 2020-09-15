e3b36d43ab9b4a47c937a613998ef9b28e928d88
antaxiom <40070530+antaxiom@users.noreply.github.com>
2020-09-15 07:39:32 -0400

    autostart.sh: removed second monitor setup

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -5,5 +5,3 @@ nitrogen --restore &
 dunst &
 killall dwmblocks
 ~/suckless/dwmblocks/dwmblocks &
-# Setup secondary monitor
-xrandr --output HDMI-A-0 --mode 1920x1080 --same-as DisplayPort-2 --scale 2.0x2.0
