921a89ce18fea0cef251774308cd769cfe497658
antaxiom <40070530+antaxiom@users.noreply.github.com>
2020-09-18 13:56:30 -0400

    sxhkd, blocks: Friendship with spotify ended, mpd is my new friend

--- suckless/dwmblocks/blocks.def.h
+++ suckless/dwmblocks/blocks.def.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "~/.scripts/status/corona",					420,		0},
-	{" ", "~/.scripts/status/spotifystatus/getInfo.sh song",					2,		0},
+	{" ", "mpc current | cut -d'-' -f2-",					1,		0},
 	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
 };
 
