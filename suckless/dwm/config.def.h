3780c03a50fab7e75eb58e99593a153a0d14cb71
Alex <40070530+antaxiom@users.noreply.github.com>
2020-10-07 22:41:10 -0400

    Finally figured out fractional scaling on X

--- suckless/dwm/config.def.h
+++ suckless/dwm/config.def.h
@@ -10,7 +10,7 @@ static const unsigned int gappov    = 16;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 54;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 40;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int horizpadbar        = 12;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 2;        /* vertical padding for statusbar */
 static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
