785dc8d3eff30b1fb0a3a0d59ec0668d3721e080
Alex <40070530+antaxiom@users.noreply.github.com>
2020-10-30 21:02:56 -0400

    dwm: minor changes here and there, border color, ect.

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -1,32 +1,33 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 12;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 12;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 12;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 12;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 40;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const int horizpadbar        = 12;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 2;        /* vertical padding for statusbar */
-static const int usealtbar          = 0;        /* 1 means use non-dwm status bar */
-static const char *altbarclass      = "Polybar"; /* Alternate bar class name */
-static const char *alttrayname      = "tray";    /* Polybar tray instance name */
-static const char *fonts[]          = { "JetBrains Mono Medium:antialias=true:autohint=true", "Noto Sans CJK JP:antialias=true:autohint=true","JetBrainsMonoNF:size=15:antialias=true:autohint=true" };
-static const char dmenufont[]       = "JetBrainsMonoNF";
-static const char col_gray1[]       = "#111111";
-static const char col_gray2[]       = "#222222";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#61afef";
-static const char *colors[][3]      = {
+static const unsigned int borderpx = 3;        /* border pixel of windows */
+static const unsigned int snap     = 32;       /* snap pixel */
+static const unsigned int gappih   = 16;       /* horiz inner gap between windows */
+static const unsigned int gappiv   = 16;       /* vert inner gap between windows */
+static const unsigned int gappoh   = 16;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov   = 16;       /* vert outer gap between windows and screen edge */
+static       int smartgaps         = 0;        /* 1 means no outer gap when there is only one window */
+static const int showbar           = 1;        /* 0 means no bar */
+static const int topbar            = 1;        /* 0 means bottom bar */
+static const int user_bh           = 40;        /* 0 means that dwm will calculate bar height, >        = 1 means dwm will user_bh as bar height */
+static const int horizpadbar       = 12;        /* horizontal padding for statusbar */
+static const int vertpadbar        = 2;        /* vertical padding for statusbar */
+static const int usealtbar         = 0;        /* 1 means use non-dwm status bar */
+static const char *altbarclass     = "Polybar"; /* Alternate bar class name */
+static const char *alttrayname     = "tray";    /* Polybar tray instance name */
+static const char *fonts[]         = { "JetBrains Mono Medium:antialias                                 = true:autohint                             = true", "Noto Sans CJK JP:antialias = true:autohint = true","JetBrainsMonoNF:size = 15:antialias = true:autohint = true" };
+static const char dmenufont[]      = "JetBrainsMonoNF";
+static const char col_gray1[]      = "#111111";
+static const char col_gray2[]      = "#222222";
+static const char col_gray3[]      = "#454545";
+static const char col_gray4[]      = "#eeeeee";
+static const char col_cyan[]       = "#61afef";
+static const char col_magenta[]    = "#409CBA";
+static const char *colors[][3]     = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray3 },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_magenta  },
 	[SchemeStatus]  = { col_cyan, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
 	[SchemeTagsSel]  = { col_gray1, col_cyan,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
     [SchemeTagsNorm]  = { col_cyan, col_gray1,  "#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
@@ -80,7 +81,7 @@ static const Layout layouts[] = {
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
 	{ MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
-	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
+	{ MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} }, \
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
