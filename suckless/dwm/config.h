7a0e2799e7f722fc19aec772b5b7020e1f21c1c8
Alex <40070530+antaxiom@users.noreply.github.com>
2020-11-02 11:13:43 -0400

    dwm / dmenu: top bar size

--- suckless/dwm/config.h
+++ suckless/dwm/config.h
@@ -10,9 +10,9 @@ static const unsigned int gappov   = 16;       /* vert outer gap between windows
 static       int smartgaps         = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar           = 1;        /* 0 means no bar */
 static const int topbar            = 1;        /* 0 means bottom bar */
-static const int user_bh           = 40;        /* 0 means that dwm will calculate bar height, >        = 1 means dwm will user_bh as bar height */
-static const int horizpadbar       = 12;        /* horizontal padding for statusbar */
-static const int vertpadbar        = 2;        /* vertical padding for statusbar */
+static const int user_bh           = 46;        /* 0 means that dwm will calculate bar height, >        = 1 means dwm will user_bh as bar height */
+static const int horizpadbar       = 16;        /* horizontal padding for statusbar */
+static const int vertpadbar        = 6;        /* vertical padding for statusbar */
 static const int usealtbar         = 0;        /* 1 means use non-dwm status bar */
 static const char *altbarclass     = "Polybar"; /* Alternate bar class name */
 static const char *alttrayname     = "tray";    /* Polybar tray instance name */
@@ -88,7 +88,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-p", "run","-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray1, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-p", "run","-m", dmenumon, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
--- suckless/dwmblocks/blocks.def.h
+++ suckless/dwmblocks/blocks.def.h
@@ -8,4 +8,4 @@ static const Block blocks[] = {
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static unsigned int delimLen = 3;
